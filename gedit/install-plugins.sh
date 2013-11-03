291824fdc9dbd7b546d80a8153ad79e8d61319c0
James Morgan <james.morgan.e@googlemail.com>
2013-11-03 13:00:24 +0000

    GEdit custom tools

---
+++ gedit/install-plugins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Installing JSON Formatter"
+cp -f json-validator ~/.config/gedit/tools/json-format
+
+echo "Installing JSON Validator"
+cp -f json-formatter ~/.config/gedit/tools/json-validate
+
+echo "Installing JSON Formatter"
+cp -f xml-formatter ~/.config/gedit/tools/xml-format
+
